77b94e5c53d957d8cd1fdb3b9f41ac646bdd2023
Le Minh Toan <toan.lm@briswell.com>
2017-07-31 11:31:39 +0900

    Add current source

---
+++ dial.php
@@ -0,0 +1,133 @@
+<?php
+include('menu.php');
+
+echo '<table>';
+
+$Path ='/etc/asterisk/cf/main/';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if(isset($_POST['user1'])){
+$file = substr($_POST['user1'],0,3);
+$Num = grChk($_POST['Num']);
+if(!file_put_contents($Path . $Num .'/m1/' . $file ,"")){ echo "内線[" .$file ."]を代表電番".$Num."00の代表1に追加しました";}
+}
+if(isset($_POST['user2'])){
+$file = substr($_POST['user2'],0,3);
+$Num = grChk($_POST['Num']);
+if(!file_put_contents($Path . $Num .'/m2/' . $file ,"")){ echo "内線[" .$file ."]を代表電番".$Num."00の代表2に追加しました";}
+}
+}
+if(isset($_GET['FP'])){
+$FP = substr($_GET['FP'],0,8);
+if(!preg_match('/^[1-9]\/m[1-2]\/[1-9][0-9][0-9]/',$FP)){$FP = "100";}
+$file = "/etc/asterisk/cf/main/".$FP;
+if(file_exists($file)){
+if(unlink($file)){
+echo basename($file) ."を除外しました";
+}
+}
+}
+if(isset($_GET['FPon'])){
+$file = "/etc/asterisk/cf/main/".grChk($_GET['FPon'])."/mail/on";
+if(file_exists($file)){
+if(unlink($file)){
+echo "代表電番" . grChk($_GET['FPon']) ."00の留守電を解除しました";
+}
+}
+}
+if(isset($_GET['FPoff'])){
+$file = "/etc/asterisk/cf/main/".grChk($_GET['FPoff'])."/mail/on";
+if(!file_put_contents($file,"")){
+echo "代表電番" . grChk($_GET['FPoff']) ."00の留守電を設定しました";
+}
+}
+echo '<tr><th>代表電番</th><th>種類</th><th>ステータス</th><th>変更</th><th> </th></tr>';
+for($i=1;$i<10;$i++){
+echo '<tr>';
+echo '<td rowspan=4>'. $i*100 . '</td>';
+echo '<tr><form method=post action=dial.php><td>代表1</td><td>';
+$opt = "";
+for($j=101;$j<1000;$j++){
+if(substr($j,1,2) !== '00'){
+$opt .= "<option value='{$j}'>{$j}</option>";
+}
+}
+foreach(glob($Path.$i.'/m1/*') as $file){
+if(is_file($file)){
+echo "<a href=dial.php?FP=". str_replace("/etc/asterisk/cf/main/","",$file) .">";
+echo basename($file)."</a> ";
+$opt = str_replace("<option value='" . basename($file) . "'>" . basename($file) . "</option>","",$opt);
+}
+}
+echo '</td><td><select name=user1>';
+echo $opt;
+echo '</select><button type=submit >追加</button>';
+echo "<input type='hidden' name='Num' value='{$i}'>";
+echo '</td></form></tr><tr><form method=post action=dial.php><td>代表2</td><td>';
+$opt = "";
+for($j=101;$j<1000;$j++){
+if(substr($j,1,2) !== '00'){
+$opt .= "<option value='{$j}'>{$j}</option>";
+}
+}
+foreach(glob($Path.$i.'/m2/*') as $file){
+if(is_file($file)){
+echo "<a href=dial.php?FP=". str_replace("/etc/asterisk/cf/main/","",$file) .">";
+echo basename($file)."</a> ";
+$opt = str_replace("<option value='" . basename($file) . "'>" . basename($file) . "</option>","",$opt);
+}
+}
+echo '</td><td><select name=user2>';
+echo $opt;
+echo '</select><button type=submit >追加</button>';
+echo "<input type='hidden' name='Num' value='{$i}'>";
+echo '</td></from></tr><tr><td>留守電</td><td>';
+$mailpath = $Path.$i.'/mail/*';
+$mail = "";
+$mail2 = "";
+if(isset(glob($mailpath)[0])){ $mail = glob($mailpath)[0] ;}
+if(!empty($mail)){      //on
+echo "<a href=dial.php?FPon={$i}>on</a>";
+}else{
+$mail2 = "<a href=dial.php?FPoff={$i}>off(onへ変更)</a>";
+echo '</td><td>' ;
+}
+echo $mail2.'</td><td>' ;
+echo '</td></form></tr>';
+}
+
+function grChk($gr){
+        switch ($gr){    //vs xss etc.
+        case '1':
+                return '1';
+                break;
+        case '2':
+                return '2';
+                break;
+        case '3':
+                return '3';
+                break;
+        case '4':
+                return '4';
+                break;
+        case '5':
+                return '5';
+                break;
+        case '6':
+                return '6';
+                break;
+        case '7':
+                return '7';
+                break;
+        case '8':
+                return '8';
+                break;
+        case '9':
+                return '9';
+                break;
+        default:
+                return '1';
+                break;
+        }
+}
+?>
+
---
+++ sip.php
@@ -0,0 +1,136 @@
+<?php
+include('menu.php');
+
+echo '<table>';
+
+//print_r( $_POST);
+$Path ='/etc/asterisk/cf/SIP/';
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if(isset($_POST['del'])){
+		if($_POST['del']==1){
+			$id = substr($_POST['id'],0,4);
+			if(is_file($Path.$id)){
+				if(unlink($Path.$id)){
+					echo ($id) ."を除外";
+				}
+			}
+			if(is_file($Path."register".substr($_POST['id'],3,1))){
+				if(unlink($Path."register".substr($_POST['id'],3,1))){
+					//echo ("register".substr($_POST['id'],3,1)) ."を除外しました";
+					echo "しました";
+				}
+			}
+		}
+	}else{
+		$id = substr($_POST['id'],0,4);
+		file_put_contents($Path.$id ,"[" . $id . "](base)
+username=". $_POST['user'] ."
+fromuser=". $_POST['user'] ."
+secret=" . $_POST['pass'] ."
+callgroup=" . $_POST['callgroup'] . "
+host=". $_POST['domain'] ."
+fromdomain=". $_POST['domain'] ."
+dtmfmode=". $_POST['dtmf'] ."
+");
+		file_put_contents($Path."register".substr($_POST['id'],3,1),"register=>" . $_POST['user'] . ":" . $_POST['pass'] ."@". $id ."
+");
+	}
+	include('sip-re.php');
+}
+echo '<th>No</th><th>user</th><th>pass</th><th>domain</th><th>DTMF</th><th>callgroup</th><th> </th><th> </th></tr>
+';
+
+for($i=1;$i<10;$i++){
+	foreach(glob($Path.'sip'. $i) as $file){
+		if(is_file($file)){
+			echo "<tr><form method='post' action='sip.php'>";
+			$tmpAry = explode("/",$file);
+			$tmpNum = count($tmpAry) -1;
+			echo '<tr><td>'. $i.'</td>';
+			$lines = file($file);
+			foreach($lines as $line) {      //file line
+				//echo $line."<br />";  //test
+				if(strpos($line,'username=') !==false){
+					echo '<td><input type=text name=user size=18 value='.htmlspecialchars(str_replace('username=','',$line)).'></td>';
+				}
+				if(strpos($line,'secret=') !==false){
+					echo '<td><input type=password name=pass size=18 value='.htmlspecialchars(str_replace('secret=','',$line)).'></td>';
+				}
+				if(strpos($line,'host=') !==false){
+					echo '<td><input type=text name=domain size=18 value='.htmlspecialchars(str_replace('host=','',$line)).'></td>';
+				}
+
+				if(strpos($line,'dtmfmode=') !==false){
+					$dtmf = str_replace('dtmfmode=','',$line);
+					$dtmf = str_replace(PHP_EOL,'',$dtmf);
+					$s1=''; $s2=''; $s3=''; $s4='';
+					if($dtmf =="inband"){$s1 = "selected";}
+					if($dtmf =="rfc2833"){$s2 = "selected";}
+					if($dtmf =="info"){$s3 = "selected";}
+					if($dtmf =="auto"){$s4 = "selected";}
+	
+					echo "<td><select name='dtmf' >";
+					echo "<option value='inband' {$s1}>inband</option>";
+					echo "<option value='rfc2833' {$s2}>rfc2833</option>";
+					echo "<option value='info' {$s3}>info</option>";
+					echo "<option value='auto' {$s4}>auto</option>";
+					echo "</select></td>
+";
+				}
+
+				if(strpos($line,'callgroup=') !==false){
+					$callgroup = str_replace('callgroup=','',$line);
+					$callgroup = str_replace(PHP_EOL,'',$callgroup);
+				}
+			}
+			echo "<td><select name='callgroup' >";
+			for($j = 1;$j<10 ;$j++){
+				if(strcmp($j,$callgroup)===0){
+					echo "<option value='{$j}' selected>{$j}</option>";
+				}else{
+					echo "<option value='{$j}' >{$j}</option>";
+				}
+			}
+
+			echo "</select></td>";
+			echo "<td><input type='hidden' name='id' value=".$tmpAry[$tmpNum].">";
+			echo "<button type='submit'>edit</button></td></form>";
+			//del user
+			echo "<form method='post' action='sip.php'><input type='hidden' name='del' value=1>";
+			echo "<input type='hidden' name='id' value=".$tmpAry[$tmpNum].">";
+			echo "<td><button type='submit'>del</button></td></form>";
+			echo '</tr>
+';
+		}
+	}
+
+
+	if(!is_file(($Path.'sip'. $i))){
+		echo "<tr><form method='post' action='sip.php'>";
+		echo '<tr><td>'. $i.'</td>';
+		echo '<td><input type=text name=user size=18></td>';
+		echo '<td><input type=text name=pass size=18></td>';
+		echo '<td><input type=text name=domain size=18></td>';
+
+		echo "<td><select name='dtmf' >";
+		echo "<option value='inband' >inband</option>";
+		echo "<option value='rfc2833'>rfc2833</option>";
+		echo "<option value='info' >info</option>";
+		echo "<option value='auto' >auto</option>";
+		echo "</select></td>";
+
+		echo "<td><select name='callgroup' >";
+		for($j = 1;$j<10 ;$j++){
+			echo "<option value='{$j}' >{$j}</option>";
+		}
+		echo "</select></td>";
+		echo "<td><input type='hidden' name='id' value=sip".$i.">";
+		echo "<button type='submit'>edit</button></td></form>";
+		echo '</tr>
+';
+	}
+
+}
+echo "</table>";
+?>
+
