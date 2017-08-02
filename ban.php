c7318ebe9d2b67f5e6a81b84eeddd1943f478328
Le Minh Toan <toan.lm@briswell.com>
2017-08-02 14:32:22 +0900

    着信拒否番号を追加

--- ban.php
+++ ban.php
@@ -5,60 +5,62 @@ define('MAX_PHONE_NO', 10);  // 電話番号入力行数
 $Path = 'ban/';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //file_put_contents
-    //echo grChk($_POST['id']).$_POST['mail']."<br>";       //test
-    $id = grChk($_POST['id']);
-    $phone = $_POST['phone'];
-    if (phoneChk($phone)) {
-        if (is_file($Path . $id)) {
+    if($_POST['phone']){
+        $phone = $_POST['phone'];
+        $originalPhone = $_POST['original_phone'];
+        if (phoneChk($phone)) {
             if (checkAuth() == 1) {
-                unlink($Path . $id);
-            }else{
+                if (is_file($Path . $originalPhone)) {
+                    unlink($Path . $originalPhone);
+                }
+                if (!file_put_contents($Path . $phone , "")) {
+                    echo "着信拒否番号" . $phone . "を更新しました";
+                }
+            }else {
                 echo "<b>管理者権限がありません</b><br />";
             }
+        } else {
+            echo "電話番号が正しくないようです";
         }
-        if (checkAuth() == 1) {
-            if (file_put_contents($Path . $id , $id  . "=>1234,demo," . $phone . ",,attach=yes|tz=japan|delete=yes")) {
-                echo "着信拒否番号" . $id . "番を更新しました";
-            }
-        }else{
-            echo "<b>管理者権限がありません</b><br />";
-        }
-    } else {
-        echo "電話番号が正しくないようです";
     }
 }
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_GET['id'])) {
-        $id = grChk($_GET['id']);
-        if (is_file($Path . $id )) {
-            if (checkAuth() == 1) {
-                if (unlink($Path . $id )) {
-                    echo "着信拒否番号" . $id . "番を削除しました";
+    if (isset($_GET['phone']) && $_GET['phone']) {
+        if(phoneChk($_GET['phone'])){
+            $phone = $_GET['phone'];
+            if (is_file($Path . $phone )) {
+                if (checkAuth() == 1) {
+                    if (unlink($Path . $phone )) {
+                        echo "着信拒否番号" . $phone . "を削除しました";
+                    }
+                }else{
+                    echo "<b>管理者権限がありません</b><br />";
                 }
-            }else{
-                echo "<b>管理者権限がありません</b><br />";
             }
+        } else {
+            echo "電話番号が正しくないようです";
         }
+
     }
 }
 echo '<table>';
 echo '<th>no</th><th>電話番号</th><th></th><th></th></tr>';
-for ($i = 1; $i <= MAX_PHONE_NO; $i++) {
+
+$phoneList = glob($Path . '*') ;
+for ($i = 0; $i < MAX_PHONE_NO; $i++) {
     echo "<tr><form method='POST' action='ban.php'>";
-    echo '<td>' . $i  . '</td><td>';
+    echo '<td>' . ($i+1)  . '</td><td>';
     $file = (($Path . $i ));
-    if (is_file($file)) {
-        $value = file_get_contents($file);
-        $array = explode(',', $value);
-        $value = $array[2];
+    if (isset($phoneList[$i])) {
+        $value = basename($phoneList[$i]);
     } else {
         $value = "";
     }
     echo '<input type=text name=phone size=50 value="' . $value . '">';
-    echo "</td><td><button type='submit' name='id' value=" . $i . ">edit</button></td></form>";
+    echo '<input type=hidden name=original_phone size=50 value="' . $value . '">';
+    echo "</td><td><button type='submit' name='id' value=" . $value . ">edit</button></td></form>";
     echo "</td><form method='GET' action='ban.php'><td>";
-    echo "<button type='submit' name='id' value=" . $i . ">del</button></td></form>";
+    echo "<button type='submit' name='phone' value=" . $value . ">del</button></td></form>";
     echo "</form></tr>";
 }
 echo "</table>";
@@ -67,16 +69,6 @@ echo "</table>";
 function phoneChk($phone)
 {
     return preg_match("/^[0-9]{0,15}$/", $phone);
-}
-
-// 行目のバリデーション
-function grChk($gr)
-{
-    if( false !== filter_var($gr, FILTER_VALIDATE_INT) && $gr <= MAX_PHONE_NO && $gr >= 1){
-        return $gr . '';
-    }else{
-        return '1';
-    }
 }
 
 ?>
